238f74aa7cc6802af2ab240da2343b8a4df30590
utkarshsingh17 <30478csai@gmail.com>
2024-05-26 14:03:54 -0400

    update1

--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -1,14 +1,22 @@
 export const fetchTopAlbums=async()=>{
+    try{
     const response=await fetch("https://qtify-backend-labs.crio.do/albums/top");
     const data=await response.json();
     return data;
+    }catch(e){
+        console.log(e)
+    }
 
 }
 
 export const fetchNewAlbums=async()=>{
+    try{
     const response=await fetch("https://qtify-backend-labs.crio.do/albums/new");
     const data=await response.json();
     return data;
+    }catch(e){
+        console.log(e);
+    }
 }
 export const fetchSongs=async()=>{
     const response=await fetch("https://qtify-backend-labs.crio.do/songs");
